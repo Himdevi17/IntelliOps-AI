9de3e4fac8c7162895f198f04f0ef7d707e9b634
himdevi17 <himdevithakre2728@gmail.com>
2026-09-03 13:33:33 +0530

    Add automated API testing

---
+++ backend/tests/test_main.py
@@ -0,0 +1,16 @@
+#tests/test_main.py
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+client = TestClient(app)
+
+
+def test_root():
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "IntelliOps AI API is running"
+    }
